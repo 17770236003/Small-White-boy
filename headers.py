b23e4bebe6e75435a3ed065006cc8221376a9c68
小白学python <66956822+17770236003@users.noreply.github.com>
2020-08-20 15:21:38 +0800

    Add files via upload

---
+++ headers.py
@@ -0,0 +1,7 @@
+def head():
+    head={
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36'
+    }
+
+
+    return head
